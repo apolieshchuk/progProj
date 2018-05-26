7d1559c81ed2073e01c347fe42fb46841246940b
apolieshchuk <anton2013ua@gmail.com>
2018-05-26 11:20:37 +0300

    First commit

---
+++ SeaBattle/src/Table.java
@@ -0,0 +1,32 @@
+public class Table {
+
+   private int row;
+   private int col;
+   private
+
+    Table(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+
+
+
+
+}
